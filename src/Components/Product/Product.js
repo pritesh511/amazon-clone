048d0adfa9c6306b06c74b9784352be6c87ebc25
pritesh511 <94275226+pritesh511@users.noreply.github.com>
2022-03-10 09:41:42 +0530

    make home page and set product component

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,18 +1,36 @@
 import React from "react";
+
+import StarIcon from "@mui/icons-material/Star";
 import {
   ProductBox,
   ProductDescription,
   ProductPrice,
   ProductRating,
+  ProductBottom,
+  ProductUpper,
+  ProductImg,
 } from "./Styles";
 
-const Product = () => {
+const Product = (props) => {
+  const { description, price, rating, image } = props;
   return (
     <>
       <ProductBox>
-        <ProductDescription>HelloProduct</ProductDescription>
-        <ProductPrice>$ 30.99</ProductPrice>
-        <ProductRating></ProductRating>
+        <ProductUpper>
+          <ProductDescription>{description}</ProductDescription>
+          <ProductPrice>$ {price}</ProductPrice>
+          <ProductRating>
+            {Array(rating)
+              .fill()
+              .map((item) => (
+                <StarIcon />
+              ))}
+          </ProductRating>
+        </ProductUpper>
+        <ProductBottom>
+          <ProductImg src={image}></ProductImg>
+          <button>Add to cart</button>
+        </ProductBottom>
       </ProductBox>
     </>
   );
--- src/Pages/Home/Styles.js
+++ src/Pages/Home/Styles.js
@@ -1,10 +1,23 @@
 import styled from "styled-components";
 
-const HeroWrapper = styled.div``;
+const HeroWrapper = styled.div`
+  margin-bottom: -150px;
+  mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+`;
 const HeroImage = styled.img`
   width: 100%;
   height: 100%;
 `;
-const ProductWrap = styled.div``;
+const ProductWrap = styled.div`
+  max-width: 1500px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+`;
+const ProductRow = styled.div`
+  display: flex;
+  margin: 0 5px;
+  width: 100%;
+`;
 
-export { HeroWrapper, HeroImage, ProductWrap };
+export { HeroWrapper, HeroImage, ProductWrap, ProductRow };
